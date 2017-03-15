caa03b5ae3dfa78cdc6eb54a427675007b956802
JuneCho23 <junecho.ucb@gmail.com>
2017-03-15 15:26:25 -0700

    Automation Test Scripts for Math

---
+++ Math/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Concurrent;
+
+namespace Math
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int number1;
+			number1 = 5;
+			//5
+			Console.WriteLine(number1);
+			number1 = number1 * 5;
+			//25
+			Console.WriteLine(number1);
+			number1 += 5;
+			//30
+			Console.WriteLine(number1);
+			number1 -= 10;
+			//20
+			Console.WriteLine(number1);
+			number1 *= 2;
+			//40
+			Console.WriteLine(number1);
+			number1 /= 8;
+			//5
+			Console.WriteLine(number1);
+			Console.WriteLine(--number1);
+			//4
+			Console.WriteLine(number1--);
+			Console.WriteLine(number1);
+		}		
+
+
+
+
+	}
+}
